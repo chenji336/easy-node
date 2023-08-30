fdd974eabc277ee4177ebf2c45f19d2359069eac
chenji <chenji53@crland.com.cn>
2023-08-30 13:56:42 +0800

    Web: escape

--- Web/utils/render.js
+++ Web/utils/render.js
@@ -3,20 +3,22 @@
  * <%=username%> => obj.username
  */
 
+const { escape } = require('./xss')
+
 const compile = (str) => {
   let tpl = str.replace(/<%=([\s\S]+?)%>/g, (match, code) => {
     // console.log(match, code)
-    return `' + ${code} + '`
+    return `' + escape(${code}) + '`
   })
   tpl = `with(obj) {return '${tpl}'}`
   console.log('tpl:', tpl)
- return new Function('obj', tpl)
+  return new Function('obj', 'escape', tpl)
 }
 
 // render 为什么拆分成 compile，而不是写在一起？
 // 缓存。执行后生成 compiled，不需要每次都生成 compiled
 const render = (compiled, data) => {
-  const res = compiled(data)
+  const res = compiled(data, escape)
   console.log('res:', res)
   return res
 }
@@ -24,9 +26,9 @@ const render = (compiled, data) => {
 render(
   compile('Hello <%=username%>'),
   {
-    username: 'chenji'
+    username: 'chenji <script>' // 执行 escape
   }
-) // => Hello chenji
+) // => Hello chenji &lt;script&gt;
 
 // 问题，不能映射普通字符
 // 可以通过 witch 解决
--- Web/utils/xss.js
+++ Web/utils/xss.js
@@ -2,3 +2,12 @@
 // 如果真的被获取了 cookie，如果后台会通过检查 **浏览器的部分信息做为加密**（除非浏览器信息一样），那还是可以防住
 
 // 一般处理方式：特殊字符转换过滤
+const escape = html => {
+  return String(html)
+    .replace(/&(?!\w+)/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+exports.escape = escape
