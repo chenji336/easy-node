9dbf1b0fd1e0487499d223a57f73516177c9207f
chenji <chenji53@crland.com.cn>
2023-09-08 15:33:37 +0800

    chore: import json 验证

--- Module/module-exports-require/getJson.js
+++ Module/module-exports-require/getJson.js
@@ -1,6 +1,10 @@
 /**
  * json 不需要导出，直接使用
  */
- const json = require('./package.json')
+const json = require('./package.json');
 
- console.log(json)
+console.log(json);
+
+// import 直接引入会报错
+// import json2 from ('./package.json')
+// console.log(json2);
